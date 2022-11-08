ad107e905dfab212e688b0b5270686b881d1988f
amna <antoniomiguel2001@hotmail.es>
2022-11-08 19:07:02 +0100

    new file:   .vscode/launch.json

--- db/usuario.php
+++ db/usuario.php
@@ -16,7 +16,7 @@ class Usuario extends DB{
         $pass_a_md5 = md5($pwd);
 
         $query = $this->conn()->prepare('SELECT * FROM usuario WHERE username = :user AND password = :pwd');
-        $query -> execute(['user'=> $user,'pwd'=> $pass_a_md5]);
+        $query -> execute(['user'=> $user,'pwd'=> $pwd]);
 
         if($query ->rowCount()){
             return true;
